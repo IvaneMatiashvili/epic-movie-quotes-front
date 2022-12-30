3cade1ff3f83742f1bd915f214005cef71a8e8a6
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-30 19:57:54 +0400

    feat: create a regitration form with validation

--- components/landingPage/landingPageComponents/registerForm/useRegisterForm.tsx
+++ components/landingPage/landingPageComponents/registerForm/useRegisterForm.tsx
@@ -1,6 +1,12 @@
 import { setStateType } from 'types'
+import { useTranslation } from 'next-i18next'
+import { useForm, useWatch } from 'react-hook-form'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const useRegisterForm = () => {
+  const { locale } = useRouter()
+
   const returnScrollbarAndCloseRegisterForm = (
     setHasScrollBar: setStateType<boolean>,
     setIsRegisterOn: setStateType<boolean>
@@ -9,7 +15,54 @@ export const useRegisterForm = () => {
     setIsRegisterOn(false)
     localStorage.setItem('registerOn', 'false')
   }
+
+  const { t } = useTranslation()
+
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirm_password: '',
+      mode: 'all',
+    },
+  })
+  const { errors, isValid } = form.formState
+
+  const watchPassword = useWatch({
+    control: form.control,
+    name: 'password',
+  })
+  const watchPasswordConfirmation = useWatch({
+    control: form.control,
+    name: 'confirm_password',
+  })
+
+  useEffect(() => {
+    if (watchPasswordConfirmation.length >= 8) {
+      watchPassword === watchPasswordConfirmation
+        ? form.clearErrors('confirm_password')
+        : form.setError('confirm_password', {
+            type: 'custom',
+            message: t('errors:passwordConfirmation')!,
+          })
+    }
+  }, [watchPassword, watchPasswordConfirmation, isValid, form, t])
+
+  const [isTypePassword, setIsTypePassword] = useState(true)
+  const [isTypeConfirmPassword, setIsTypeConfirmPassword] = useState(true)
+
   return {
     returnScrollbarAndCloseRegisterForm,
+    t,
+    errors,
+    isTypePassword,
+    setIsTypePassword,
+    isTypeConfirmPassword,
+    setIsTypeConfirmPassword,
+    handleSubmit: form.handleSubmit,
+    form,
+    watchPassword,
+    locale,
   }
 }
