0ef6cfd00ae402800349cec1aa8fbc1eb2dcdcb0
IvaneMatiashvili <ivanematiashvili@gmail.com>
2023-02-16 21:00:19 +0400

    feat: create a markap for movies page errors

---
+++ components/shared/form/MoviesInputError/MoviesInputError.tsx
@@ -0,0 +1,29 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { MoviesErrorProps } from './types'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const MoviesInputError: React.FC<MoviesErrorProps> = (props) => {
+  const { locale } = useRouter()
+  return (
+    <ErrorMessage
+      errors={props.errors}
+      name={props.name}
+      render={({ message }) => (
+        <div className='flex justify-start'>
+          <div className='w-r19 nm:w-r22 sm:w-r24'>
+            <p
+              className={`absolute mt-0.2 text-xs font-light text-redStar ${
+                locale === 'en' ? 'font-helveticaEn' : 'font-helveticaKa'
+              }`}
+            >
+              {message}
+            </p>
+          </div>
+        </div>
+      )}
+    />
+  )
+}
+
+export default MoviesInputError
---
+++ components/shared/form/MoviesInputError/index.ts
@@ -0,0 +1,1 @@
+export { default as MoviesInputError } from './MoviesInputError'
