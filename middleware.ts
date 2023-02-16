ffcc0ba2392c7c2de7827c5490c30925ebc3a6e9
IvaneMatiashvili <ivanematiashvili@gmail.com>
2023-02-16 20:55:06 +0400

    feat: add movies routes

--- middleware.ts
+++ middleware.ts
@@ -20,5 +20,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/profile', '/verify-user'],
+  matcher: ['/', '/profile', '/verify-user', '/movies', '/403', '/404'],
 }
---
+++ pages/movies/[movie]/quote/[quote].tsx
@@ -0,0 +1,33 @@
+import { MovieDescriptionMain, ViewQuote } from 'components'
+import { Locale } from 'types'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticPaths } from 'next'
+
+const Index = () => {
+  return (
+    <>
+      <ViewQuote />
+    </>
+  )
+}
+export const getStaticProps = async ({ locale }: Locale) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'profile',
+        'common',
+        'errors',
+        'movies',
+      ])),
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export default Index
