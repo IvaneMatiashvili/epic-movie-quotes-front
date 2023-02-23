dda9127d42f44f5f74893ad04fabab8937f503f0
IvaneMatiashvili <ivanematiashvili@gmail.com>
2023-02-23 04:19:55 +0400

    feat: add news feed service

---
+++ services/newsFeedService.ts
@@ -0,0 +1,19 @@
+import axios from './axios'
+export const getQuotes = async (start: number) => {
+  return await axios.get(`/api/get-quotes?start=${start}`)
+}
+
+export const getNotifications = async (start: number) => {
+  return await axios.get(`/api/get-notifications?start=${start}`)
+}
+export const searchQuotes = async (data: object) => {
+  return await axios.post('/api/search-quotes', data)
+}
+
+export const removeNotifications = async () => {
+  return await axios.get('/api/remove-notifications')
+}
+
+export const removeNotification = async (data: object) => {
+  return await axios.post('/api/remove-notification', data)
+}
